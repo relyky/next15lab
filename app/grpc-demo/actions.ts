dd8ffdffadc24bff7e97df81860cd43f77cf0470
Rely Ky <relyky@hotmail.com>
2025-06-19 13:04:57 +0800

    transportInsecure

--- app/grpc-demo/actions.ts
+++ app/grpc-demo/actions.ts
@@ -5,16 +5,18 @@ import { GreeterClient } from "@/generated/greet.client";
 import { ChannelCredentials } from "@grpc/grpc-js";
 import { GrpcTransport } from "@protobuf-ts/grpc-transport";
 
-// host: '172.17.0.2:8080', 不行
-// host: 'localhost:32768', // ok, createInsecure
-// host: 'localhost:32769', 
-const transport = new GrpcTransport({
+const transportInsecure = new GrpcTransport({
   host: 'localhost:32768', // 替換為你的 gRPC 服務地址
   channelCredentials: ChannelCredentials.createInsecure(), // 未加入安全機制
 });
 
+const transportSsl = new GrpcTransport({
+  host: 'localhost:32769', // 替換為你的 gRPC 服務地址
+  channelCredentials: ChannelCredentials.createSsl(), // 使用預設的憑證(但 docker 本身沒有憑證!需額外再配製)
+});
+
 // 創建客戶端實例
-const client = new GreeterClient(transport);
+const client = new GreeterClient(transportInsecure);
 
 export async function handleSayHello() {
 
