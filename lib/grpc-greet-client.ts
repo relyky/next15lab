bc1d0e3bf14c458d9d3e6d8f39808c40ef272d75
Rely Ky <relyky@hotmail.com>
2025-06-19 16:26:46 +0800

    環境參數

--- lib/grpc-greet-client.ts
+++ lib/grpc-greet-client.ts
@@ -9,12 +9,12 @@ import { ChannelCredentials } from "@grpc/grpc-js";
 import { GrpcTransport } from "@protobuf-ts/grpc-transport";
 
 const transportInsecure = new GrpcTransport({
-  host: 'localhost:32768', // 替換為你的 gRPC 服務地址
+  host: process.env.GREETER_HOST_INSECURE!, // 替換為你的 gRPC 服務地址
   channelCredentials: ChannelCredentials.createInsecure(), // 未加入安全機制
 });
 
 const transport = new GrpcTransport({
-  host: 'localhost:32769', // 替換為你的 gRPC 服務地址
+  host: process.env.GREETER_HOST!, // 替換為你的 gRPC 服務地址
   channelCredentials: ChannelCredentials.createSsl(), // 使用預設的憑證(但 docker 本身沒有憑證!需額外再配製)
 });
 
